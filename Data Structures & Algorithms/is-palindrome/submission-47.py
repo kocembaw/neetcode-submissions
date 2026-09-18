8ec35cea13e673b1129e3ed5882001b1bfbb3e1d
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-09-18 21:30:04 +0200

    Add: is-palindrome - submission-47

---
+++ Data Structures & Algorithms/is-palindrome/submission-47.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 0:
+            return False
+
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l] != s[r]:
+                return False
+        return True
