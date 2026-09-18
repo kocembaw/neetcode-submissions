99d37505f723775957c775c884f70e1f6bc8a769
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-09-18 20:00:54 +0200

    Add: buy-and-sell-crypto - submission-28

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-28.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        max_profit = 0
+        max_price = 0
+        for price in prices:
+            if price > max_price:
+                max_profit = max(max_profit, max_price - price)
+        
+
+        return max_price
+
