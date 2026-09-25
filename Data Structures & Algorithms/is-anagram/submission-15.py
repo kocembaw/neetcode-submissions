5c47fa3aeac091ef87b671179b66d92d6073829d
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-09-25 13:30:07 +0200

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        seen_s = {}
+        seen_t = {}
+        for chs, cht in zip(s,t):
+            seen_s[chs] = seen_s.get(chs, 0) + 1
+            seen_t[cht] = seen_t.get(cht, 0) + 1
+        return seen_s == seen_t
